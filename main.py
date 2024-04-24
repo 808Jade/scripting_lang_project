de36370285fa86ffa79463ad5c8af9188820dc58
MainPC <129085079+808Jade@users.noreply.github.com>
2024-04-24 19:42:55 +0900

    캘린더 이미지로 대체, 요일 출력

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ window.geometry("480x640+400+300")
 window.resizable(False, False)
 
 ########## menu interface label ##########
+def update_menu_interface(event=None):
+    pass
+
 menu_interface_label = Label(master=window, width=100)
 menu_interface_label.place(x=40,y=20,anchor="center")
 
@@ -35,7 +38,7 @@ def direct_input(event=None):
     new_text = entry.get()
     logging.info(f'new input : {new_text}')
 
-direct_input_label = Label(master=window, text="직접 입력하기(Enter)", font=('Consolas',12))
+direct_input_label = Label(master=window, text="메뉴 직접 입력하기(Enter)", font=('Consolas',12))
 direct_input_label.place(x=20,y=490)
 direct_input_box = Frame(master=window, borderwidth=5, width=30, height=1, relief=GROOVE)
 entry = Entry(direct_input_box, font=('Consolas',12))
@@ -45,16 +48,19 @@ direct_input_box.place(x=20,y=510)
 
 
 ########## date label ##########
-today = datetime.today().strftime("%Y년 %m월 %d일")
-todays_date_label = Label(master=window, text=today, font=('Consolas', 20), background='skyblue')
-todays_date_label.place(x=20,y=550)
+today = datetime.today().strftime("%Y년 %m월 %d일(%a)")
+todays_date_label = Label(master=window, text=today, font=('Consolas', 20), background='#FF9933')
+todays_date_label.place(x=20,y=590)
 
 ########## Calendar button ##########
 def move_to_calendar(event=None):
     logging.info(f'calendar button clicked')
 
-calendar_button = Button(master=window, text="캘린더로 이동", width=19, padding=35, command=move_to_calendar)
-calendar_button.place(x=250, y=490)
+calendar_image = PhotoImage(file='calendar_icon.png')
+calendar_image = calendar_image.subsample(4, 4)
+
+calendar_button = Button(master=window, image=calendar_image, command=move_to_calendar)
+calendar_button.place(x=320, y=490)
 
 
 window.bind('<Escape>', window.quit())
