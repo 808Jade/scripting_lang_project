24772fc5943bad3fef6a3a3f1339865554ec64ed
MainPC <129085079+808Jade@users.noreply.github.com>
2024-04-26 00:09:10 +0900

    추천버튼/아점저버튼 class화

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ import tkinter.ttk as ttk
 from tkinter.scrolledtext import ScrolledText
 from datetime import datetime
 
+
+########## window ##########
 window = tk.Tk()
 window.title("점메추")
 window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 
-
 window.geometry("480x640+400+300")
 window.resizable(False, False)
+
+
+########## Global Variables ##########
+
 
 ########## menu interface label ##########
 def update_menu_interface(event=None):
@@ -25,12 +30,7 @@ def update_menu_interface(event=None):
 menu_interface_label = ttk.Label(master=window, width=100)
 menu_interface_label.place(x=40,y=20,anchor="center")
 
-########## recommend button ##########
-def recommend(event=None):
-    logging.info(f'recommend button clicked')
 
-recommend_button = ttk.Button(master=window, text="추천 받기!", width=30, padding=20, command=recommend)
-recommend_button.place(x=240, y=320, anchor="center")
 
 
 ########## direct Input box ##########
@@ -53,6 +53,7 @@ today = datetime.today().strftime("%Y년 %m월 %d일(%a)")
 todays_date_label = ttk.Label(master=window, text=today, font=('Consolas', 20))
 todays_date_label.place(x=20,y=570)
 
+
 ########## Calendar button ##########
 def move_to_calendar(event=None):
     logging.info(f'calendar button clicked')
@@ -63,7 +64,51 @@ calendar_image = calendar_image.subsample(4, 4)
 calendar_button = tk.Button(master=window, image=calendar_image, command=move_to_calendar, bg="#F0F0F0", cursor='right_side',borderwidth=0, highlightthickness=0)
 calendar_button.place(x=330, y=490)
 
+
 ########## 아/점/저 Radiobutton ##########
+class BreakfastButton(tk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.image = tk.PhotoImage(file='breakfast_icon.png')
+        self.image = self.image.subsample(6, 6)
+        self['image'] = self.image
+        self.place(x=95, y=370)
+        self['command'] = self.clicked
+
+    def clicked(self):
+        logging.info('breakfast_button_clicked')
+
+
+class LunchButton(tk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.image = tk.PhotoImage(file='lunch_icon.png')
+        self.image = self.image.subsample(6, 6)
+        self['image'] = self.image
+        self.place(x=195, y=370)
+        self['command'] = self.clicked
+
+    def clicked(self):
+        logging.info('Lunch_button_clicked')
+
+
+class DinnerButton(tk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.image = tk.PhotoImage(file='dinner_icon.png')
+        self.image = self.image.subsample(6, 6)
+        self['image'] = self.image
+        self.place(x=295, y=370)
+        self['command'] = self.clicked
+
+    def clicked(self):
+        logging.info('Dinner_button_clicked')
+
+breakfast_button = BreakfastButton()
+lunch_button = LunchButton()
+dinner_button = DinnerButton()
+
+
 def init_bld_index():
     current_time = datetime.now().time()
     hour = current_time.hour
@@ -78,34 +123,82 @@ def init_bld_index():
 BLD_index = init_bld_index()
 
 def breakfast_button_clicked(event=None):
+    logging.info('breakfast_button_clicked')
     global BLD_index
     BLD_index = 1
-    logging.info(f'breakfast_button_clicked / index - {BLD_index}')
+    recommend_button.update_recommendation()  # 버튼 클릭시 텍스트 업데이트
 
-breakfast_image = tk.PhotoImage(file='breakfast_icon.png')
-breakfast_image = breakfast_image.subsample(6,6)
-breakfast_button = tk.Button(master=window, image=breakfast_image, command=breakfast_button_clicked)
-breakfast_button.place(x=95,y=370)
 
 def lunch_button_clicked(event=None):
+    logging.info('lunch_button_clicked')
     global BLD_index
     BLD_index = 2
-    logging.info(f'lunch_button_clicked / index - {BLD_index}')
+    recommend_button.update_recommendation()  # 버튼 클릭시 텍스트 업데이트
 
-lunch_image = tk.PhotoImage(file='lunch_icon.png')
-lunch_image = lunch_image.subsample(6,6)
-lunch_button = tk.Button(master=window, image=lunch_image, command=lunch_button_clicked)
-lunch_button.place(x=195,y=370)
 
 def dinner_button_clicked(event=None):
+    logging.info('dinner_button_clicked')
     global BLD_index
     BLD_index = 3
-    logging.info(f'dinner_button_clicked / index - {BLD_index}')
+    recommend_button.update_recommendation()  # 버튼 클릭시 텍스트 업데이트
 
-dinner_image = tk.PhotoImage(file='dinner_icon.png')
-dinner_image = dinner_image.subsample(6,6)
-dinner_button = tk.Button(master=window, image=dinner_image, command=dinner_button_clicked)
-dinner_button.place(x=295,y=370)
+# def breakfast_button_clicked(event=None):
+#     global BLD_index
+#     BLD_index = 1
+#     logging.info(f'breakfast_button_clicked / index - {BLD_index}')
+#     get_bld_index()
+# def lunch_button_clicked(event=None):
+#     global BLD_index
+#     BLD_index = 2
+#     logging.info(f'lunch_button_clicked / index - {BLD_index}')
+#     get_bld_index()
+# def dinner_button_clicked(event=None):
+#     global BLD_index
+#     BLD_index = 3
+#     logging.info(f'dinner_button_clicked / index - {BLD_index}')
+#     get_bld_index()
+
+########## recommend button ##########
+class RecommendButton(ttk.Button):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.update_recommendation()  # 초기 텍스트 업데이트
+
+    def update_recommendation(self):
+        now = datetime.now().time()
+        hour = now.hour
+        if 6 <= hour < 11:
+            self.config(text="아침 추천 받기!")
+        elif 11 <= hour < 17:
+            self.config(text="점심 추천 받기!")
+        else:
+            self.config(text="저녁 추천 받기!")
+
+    def recommend(self):
+        logging.info(f'recommend button clicked')
+
+
+
+#
+# def recommend(event=None):
+#     logging.info(f'recommend button clicked')
+
+# def get_bld_index(event=None):
+#     global BLD_index
+#     now = "None"
+#     if BLD_index == 1:
+#         return "아침 추천 받기!"
+#     elif BLD_index == 2:
+#         return "점심 추천 받기!"
+#     elif BLD_index == 3:
+#         return "저녁 추천 받기!"
+
+recommend_button = RecommendButton(master=window, width=30, padding=20)
+recommend_button.config(command=recommend_button.recommend)
+
+recommend_button.place(x=240, y=320, anchor="center")
+# recommend_button = ttk.Button(master=window, text=get_bld_index(), width=30, padding=20, command=recommend)
+# recommend_button.place(x=240, y=320, anchor="center")
 
 
 window.bind('<Escape>', lambda event: window.quit())
